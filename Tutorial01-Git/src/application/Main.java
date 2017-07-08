78a3c1c32b4be9a07adb27d2180e70bc0582866e
Nathan Leach <nleach999@gmail.com>
2017-07-08 14:54:11 -0500

    Breaking 2 drawings for merge conflict example.

--- Tutorial01-Git/src/application/Main.java
+++ Tutorial01-Git/src/application/Main.java
@@ -44,8 +44,7 @@ public class Main extends Application implements TutorialEventListener {
 
 	private Canvas _mainDrawingArea = new Canvas();
 	
-	private ObservableList<String> _drawOptions = FXCollections.observableArrayList("Mandlebrot", 
-			"Sierpinski Gasket", "Walker");
+	private ObservableList<String> _drawOptions = FXCollections.observableArrayList("Mandlebrot");
 	
 	private ComboBox<String> _drawingSelection = new ComboBox<String>(_drawOptions);
 	private Button _stopButton, _startButton, _clearButton;
@@ -110,12 +109,6 @@ public class Main extends Application implements TutorialEventListener {
 
 			if (selectedDrawing.compareTo("Mandlebrot") == 0)
 				_runningDrawing = new Mandlebrot();
-
-			 if (selectedDrawing.compareTo("Sierpinski Gasket") == 0)
-					_runningDrawing = new Sierpinski();
-
-			 if (selectedDrawing.compareTo("Walker") == 0)
-					_runningDrawing = new Walker();
 
 			if (_runningDrawing != null) {
 				_stopButton.setDisable(false);
